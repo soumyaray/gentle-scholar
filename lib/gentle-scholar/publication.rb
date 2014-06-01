ba835206b967d77db15500487a1b3c1a7ca3bff5
Soumya Ray <soumya.ray@gmail.com>
2014-06-01 23:30:40 +0800

    refactored long methods and case statements with hashed lambdas

--- lib/gentle-scholar/publication.rb
+++ lib/gentle-scholar/publication.rb
@@ -1,13 +1,12 @@
 # Author::       Soumya Ray (mailto: soumya.ray@gmail.com)
 # License::      MIT
-
 require 'typhoeus'
 require 'nokogiri'
 require 'date'
 
-# This class loads a single publication from Google scholar and returns
-# all its attributes, including dynamic attributes like number of citations
 module GentleScholar
+  # This class loads a single publication from Google scholar and returns
+  # all its attributes, including dynamic attributes like number of citations
   class Publication
     GSCHOLAR_HOST_URL = 'http://scholar.google.com'
     GSCHOLAR_CIT_URL =
@@ -23,6 +22,15 @@ module GentleScholar
       chart_url: '//div[contains(@class,"cit-dd")]/img'
     }
 
+    SCAN_LAMBDAS = {
+      cites:         ->(x) { x.text[/\d+/].to_i },
+      cites_url:     ->(x) { x[0].attributes['href'].value },
+      title:         ->(x) { x.text },
+      article_url:   ->(x) { x.attr('href').value },
+      chart_url:     ->(x) { x.attr('src').value },
+      gscholar_url:  ->(x) { GSCHOLAR_HOST_URL + x.attr('href').value }
+    }
+
     TABLE_ATTR = {
       authors: 'Authors',
       date: 'Publication date',
@@ -32,6 +40,11 @@ module GentleScholar
       pages: 'Pages',
       publisher: 'Publisher',
       description: 'Description'
+    }
+
+    TABLE_LAMBDAS = {
+      authors:  ->(x) { x.split(/,/).map { |a| a.split(' ') } },
+      date:     ->(x) { Date.strptime(x, '%Y/%m/%d') }
     }
 
     def self.get_from_http(scholar_pub_id)
@@ -45,39 +58,20 @@ module GentleScholar
     end
 
     def self.extract_html_elements(doc)
-      xpath = {}
-      elements = {}
-      SCAN_STR.each do |elem, _path|
-        xpath[elem] = doc.xpath(SCAN_STR[elem])
-        elements[elem] = ''
-      end
-
-      elements[:cites] = xpath[:cites].text[/\d+/].to_i
-      elements[:cites_url] = xpath[:cites_url][0].attributes['href'].value
-
-      elements[:title] = xpath[:title].text
-      elements[:article_url] = xpath[:article_url].attr('href').value
-
-      elements[:chart_url] = xpath[:chart_url].attr('src').value
-
-      elements[:gscholar_url] = GSCHOLAR_HOST_URL \
-                                + xpath[:gscholar_url].attr('href').value
-
-      elements
+      xpath = Hash[SCAN_STR.map { |elem, path| [elem, doc.xpath(path)] }]
+      Hash[SCAN_LAMBDAS.map { |key, lam| [key, lam.call(xpath[key])] }]
     end
 
     def self.extract_html_table(doc)
       # lambda gets text from right html column given name in left column
-      table_pick = lambda do |name|
+      table_extract = lambda do |name|
         doc.xpath("//div[starts-with(.,'#{name}')]")[0].children[1].text
       end
 
-      elements = Hash[ TABLE_ATTR.map { |k, v| [k, table_pick.call(v)] } ]
-
-      elements[:authors] = elements[:authors].split(/,/)\
-                                             .map { |a| a.split(' ') }
-      elements[:date] = Date.strptime(elements[:date], '%Y/%m/%d')
-      elements
+      elements = Hash[TABLE_ATTR.map { |k, v| [k, table_extract.call(v)] }]
+      elements.merge(
+        Hash[TABLE_LAMBDAS.map { |key, lam| [key, lam.call(elements[key])] }]
+      )
     end
   end
 end
