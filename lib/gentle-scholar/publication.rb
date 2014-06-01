94c76a28b94ad978b2c92fa190819c3c98c2cc41
Soumya Ray <soumya.ray@gmail.com>
2014-06-01 15:13:19 +0800

    get_from_http now returns Hash, resolves #2; refactoring extract_html_elements refactoring extract_html_table

--- lib/gentle-scholar/publication.rb
+++ lib/gentle-scholar/publication.rb
@@ -13,16 +13,11 @@ module GentleScholar
     GSCHOLAR_CIT_URL =
         'http://scholar.google.com/citations?view_op=view_citation&hl=en'
 
-    attr_reader :title, :cites, :cites_url, :chart_url, :article_url
-    attr_reader :authors, :date, :journal, :volume, :issue, :pages, :publisher
-    attr_reader :description, :gscholar_url
-    # TODO: @doc only for development, testing modes
-    attr_reader :doc
-
     SCAN_STR = {
       gscholar_url:
         "//div[contains(@class,'g-section cit-dgb')]/div/table/tr/td/a",
       cites: "//div[contains(@id,'scholar_sec')]/div/a",
+      cites_url: "//div[contains(@id,'scholar_sec')]/div/a",
       title: '//div[@id="title"]/a',
       article_url: '//div[@id="title"]/a',
       chart_url: '//div[contains(@class,"cit-dd")]/img'
@@ -39,42 +34,50 @@ module GentleScholar
       description: 'Description'
     }
 
-    def initialize(scholar_pub_id)
+    def self.get_from_http(scholar_pub_id)
       auth_id, pub_id = scholar_pub_id.split(/:/)
       url = GSCHOLAR_CIT_URL + '&user=' + auth_id \
                              + '&citation_for_view=' + auth_id + ':' + pub_id
       res = Typhoeus::Request.new(url).run
-      @doc = Nokogiri::HTML(res.response_body)
+      doc = Nokogiri::HTML(res.response_body)
 
-      extract_html_elements
-      extract_html_table
+      extract_html_elements(doc).merge(extract_html_table(doc))
     end
 
-    def extract_html_elements
-      @cites = @doc.xpath(SCAN_STR[:cites]).text[/\d+/].to_i
-      @cites_url = @doc.xpath(SCAN_STR[:cites])[0].attributes['href'].value
+    def self.extract_html_elements(doc)
+      xpath = {}
+      elements = {}
+      SCAN_STR.each do |elem, _path|
+        xpath[elem] = doc.xpath(SCAN_STR[elem])
+        elements[elem] = ''
+      end
 
-      @title = @doc.xpath(SCAN_STR[:title]).text
-      @article_url = @doc.xpath(SCAN_STR[:article_url]).attr('href').value
+      elements[:cites] = xpath[:cites].text[/\d+/].to_i
+      elements[:cites_url] = xpath[:cites_url][0].attributes['href'].value
 
-      @chart_url = @doc.xpath(SCAN_STR[:chart_url]).attr('src').value
+      elements[:title] = xpath[:title].text
+      elements[:article_url] = xpath[:article_url].attr('href').value
 
-      @gscholar_url = GSCHOLAR_HOST_URL + @doc.xpath(
-                                    SCAN_STR[:gscholar_url]).attr('href').value
+      elements[:chart_url] = xpath[:chart_url].attr('src').value
+
+      elements[:gscholar_url] = GSCHOLAR_HOST_URL \
+                                + xpath[:gscholar_url].attr('href').value
+
+      elements
     end
 
-    def extract_html_table
+    def self.extract_html_table(doc)
       # lambda gets text from right html column given name in left column
       table_pick = lambda do |name|
-        @doc.xpath("//div[starts-with(.,'#{name}')]")[0].children[1].text
+        doc.xpath("//div[starts-with(.,'#{name}')]")[0].children[1].text
       end
 
-      TABLE_ATTR.each do |k, v|
-        instance_variable_set("@#{k}", table_pick.call(v))
-      end
+      elements = Hash[ TABLE_ATTR.map { |k, v| [k, table_pick.call(v)] } ]
 
-      @authors = @authors.split(/,/).map { |a| a.split(' ') }
-      @date = Date.strptime(@date, '%Y/%m/%d')
+      elements[:authors] = elements[:authors].split(/,/)\
+                                             .map { |a| a.split(' ') }
+      elements[:date] = Date.strptime(elements[:date], '%Y/%m/%d')
+      elements
     end
   end
 end
